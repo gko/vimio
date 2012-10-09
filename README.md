c7a9c83f21f7dc311814829652e4a01315948c0f
Konstantin Gorodinskiy <gor.konstantin@gmail.com>
2012-10-09 15:30:30 +0400

    testing new install script

--- README.md
+++ README.md
@@ -2,19 +2,9 @@
 
 ##Installation
 
-On unix:
+On unix and windows(with unix tools):
 ```
-cd&&
-rm -rf $HOME/.vim&&
-rm -rf $HOME/.vimrc&&
-rm -rf vim-settings&&
-git clone --depth 1 --recursive https://github.com/gorodinskiy/vim-settings.git&&
-mv vim-settings/_vimrc $HOME/.vimrc&&
-mv vim-settings $HOME/.vim&&
-cd .vim&&
-chmod +x bin/ctags&&
-cd&&
-vim +BundleInstall +qall
+curl -L https://raw.github.com/gorodinskiy/vim-settings/master/install.sh|sh
 ```
 
 ##Features
---
+++ install.sh
@@ -0,0 +1,21 @@
+if [[ `uname` = "Linux" || `uname` = "Darwin" ]]; then
+    cd&&
+    rm -rf $HOME/.vim&&
+    rm -rf $HOME/.vimrc&&
+    rm -rf vim-settings&&
+    git clone --depth 1 --recursive https://github.com/gorodinskiy/vim-settings.git&&
+    mv vim-settings/_vimrc $HOME/.vimrc&&
+    mv vim-settings $HOME/.vim&&
+    cd .vim&&
+    chmod +x bin/ctags&&
+    cd&&
+    vim +BundleInstall +qall
+else
+    cd&&
+    rm -rf ./vimfiles&&
+    rm -rf ./_vimrc&&
+    rm -rf ./vim-settings&&
+    git clone --depth 1 --recursive https://github.com/gorodinskiy/vim-settings.git&&
+    mv ./vim-settings/_vimrc ./_vimrc&&
+    mv ./vim-settings ./vimfiles&&
+fi
