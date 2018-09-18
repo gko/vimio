187902a392eb1a4966a960e823d46f06c1386abb
Konstantin Gorodinskiy <konstantin.gorodinskiy.digital-and-you@ext.pmu.fr>
2018-09-18 20:27:22 +0200

    [tech] initial support for nvim

--- install.sh
+++ install.sh
@@ -6,18 +6,28 @@ osname=$(uname -s)
 if [[ "$osname" == "Darwin" || "$osname" == "Linux" ]]; then
 	echo "🎩 installing «vimio»:" 
 	cd ~
+
 	echo "🚧 removing current vim settings"
-	rm -rf $HOME/.vim > /dev/null
-	rm -rf $HOME/.vimrc > /dev/null
+	rm -rf ~/.config/nvim
+	rm -rf ~/.vim > /dev/null
+	rm -rf ~/.vimrc > /dev/null
 	rm -rf vimio > /dev/null
+
 	echo "⚙️  downloading latest version"
 	git clone --depth 1 -b TECH_moveToVimPlug --recursive https://github.com/gko/vimio.git vimio
+
 	echo "⚡️ installing..."
-	mv vimio/_vimrc ~/.vimrc > /dev/null
+	cp vimio/_vimrc ~/.vimrc > /dev/null
+	mv vimio/_vimrc vimio/init.vim
 	mv vimio ~/.vim > /dev/null
+	mkdir ~/.config
+	ln -s ~/.vim ~/.config/nvim
+
 	cd .vim > /dev/null
 	mv .ctags ../ > /dev/null
 	chmod +x bin/ctags > /dev/null
 	cd ~ > /dev/null
+	cp ~ 
+
 	</dev/tty vim +PlugInstall +qall
 fi
