9adb93a787659668d849212dad22c1a372e26046
Konstantin Gorodinskii <mail@konstantin.io>
2023-07-21 22:03:56 +0200

    configure a no snippet solution for cmp
    https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#no-snippet-plugin

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -8,11 +8,33 @@ local ok, cmp = pcall(require, "cmp")
 
 if not ok then return end
 
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 cmp.setup({
+    -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#no-snippet-plugin
     snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            unpack = unpack or table.unpack
+            local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
+            local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
+            local indent = string.match(line_text, '^%s*')
+            local replace = vim.split(args.body, '\n', true)
+            local surround = string.match(line_text, '%S.*') or ''
+            local surround_end = surround:sub(col)
+
+            replace[1] = surround:sub(0, col - 1) .. replace[1]
+            replace[#replace] = replace[#replace] .. (#surround_end > 1 and ' ' or '') .. surround_end
+            if indent ~= '' then
+                for i, line in ipairs(replace) do
+                    replace[i] = indent .. line
+                end
+            end
+
+            vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
         end,
     },
 
@@ -23,9 +45,33 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        -- ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+        -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#no-snippet-plugin
+        ['<C-Space>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        },
+        ['<Tab>'] = function(fallback)
+            if not cmp.select_next_item() then
+                if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if not cmp.select_prev_item() then
+                if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
+            end
+        end,
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
