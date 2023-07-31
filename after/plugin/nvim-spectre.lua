4336ff2381f12de0ca85163e6cef7b7b2a970ef7
Konstantin Gorodinskii <mail@konstantin.io>
2023-07-31 22:25:25 +0200

    add config for spectre

---
+++ after/plugin/nvim-spectre.lua
@@ -0,0 +1,35 @@
+local ok, spectre = pcall(require, "spectre")
+
+if not ok then return end
+
+-- https://github.com/nvim-pack/nvim-spectre#usage
+vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+    desc = "Toggle Spectre"
+})
+
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+    desc = "Search current word"
+})
+
+vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+    desc = "Search current word"
+})
+
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    desc = "Search on current file"
+})
+
+-- https://github.com/nvim-pack/nvim-spectre#customization
+spectre.setup({
+    color_devicons = true,
+    open_cmd       = 'vnew',
+    live_update    = false, -- auto execute search again when you write to any file in vim
+    line_sep_start = '┌-----------------------------------------',
+    result_padding = '¦  ',
+    line_sep       = '└-----------------------------------------',
+    highlight      = {
+        ui = "String",
+        search = "DiffChange",
+        replace = "DiffDelete"
+    },
+})
